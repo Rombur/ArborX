7dc209a086ba3387f0705335d0ca07e56b86ba20
w0g <gew1@ornl.gov>
2022-01-13 05:33:46 -0500

    <WIP>initial commit for the water tight ray-triangle intersection

---
+++ src/details/ArborX_Triangle.hpp
@@ -0,0 +1,21 @@
+#ifndef ARBORX_TRIANGLE_HPP
+#define ARBORX_TRIANGLE_HPP
+
+#include <ArborX_Box.hpp>
+#include <ArborX_DetailsAlgorithms.hpp>
+#include <ArborX_Point.hpp>
+
+namespace ArborX
+{
+namespace Experimental
+{
+struct Triangle
+{
+  Point a;
+  Point b;
+  Point c;
+};
+} // namespace Experimental
+} // namespace ArborX
+
+#endif
